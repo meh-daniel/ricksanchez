52496df345bb6352dd5668e716973f28d7788be3
Danil Galkin <mehanikdanil@gmail.com>
2022-07-12 14:02:29 +0700

    added ViewHolder for characters

--- app/src/main/java/meh/daniel/com/ricksanchez/presentation/CharactersAdapter.kt
+++ app/src/main/java/meh/daniel/com/ricksanchez/presentation/CharactersAdapter.kt
@@ -4,7 +4,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import meh.daniel.com.ricksanchez.data.model.CharactersNW
+import meh.daniel.com.ricksanchez.databinding.ItemCharacterBinding
 
 class CharactersAdapter : ListAdapter<CharactersNW.Result, RecyclerView.ViewHolder>(weatherDiffUtil) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -19,6 +21,16 @@ class CharactersAdapter : ListAdapter<CharactersNW.Result, RecyclerView.ViewHold
 sealed class CharactersUI{
     data class Character(val urlAvatar: String, val name: String, val gender: String)
     data class Button(val title: String)
+}
+
+class CharacterViewHolder(private val binding: ItemCharacterBinding) : RecyclerView.ViewHolder(binding.root){
+    fun bind(item: CharactersUI.Character){
+        binding.tvName.text = item.name
+        binding.tvGender.text = item.gender
+        Glide.with(binding.ivAvatar)
+            .load(item.urlAvatar)
+            .into(binding.ivAvatar)
+    }
 }
 
 val weatherDiffUtil = object : DiffUtil.ItemCallback<CharactersNW.Result>() {
