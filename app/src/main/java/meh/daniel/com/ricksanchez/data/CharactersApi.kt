3f430f274b6e45d6a8c5db2c592c2863f8855b5a
Danil Galkin <mehanikdanil@gmail.com>
2022-07-12 11:08:41 +0700

    Создание create api

--- app/src/main/java/meh/daniel/com/ricksanchez/data/CharactersApi.kt
+++ app/src/main/java/meh/daniel/com/ricksanchez/data/CharactersApi.kt
@@ -1,6 +1,20 @@
 package meh.daniel.com.ricksanchez.data
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 const val BASE_URL = "https://rickandmortyapi.com/api/"
 
 class CharactersApi {
+
+    companion object{
+        fun createApi() : CharactersApi{
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(CharactersApi::class.java)
+        }
+    }
+
 }
