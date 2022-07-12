03131bab99244569dc8d1bf8d17801412782bd39
Danil Galkin <mehanikdanil@gmail.com>
2022-07-12 11:40:45 +0700

    Добавление mainViewModel

--- app/src/main/java/meh/daniel/com/ricksanchez/presentation/MainActivity.kt
+++ app/src/main/java/meh/daniel/com/ricksanchez/presentation/MainActivity.kt
@@ -2,12 +2,18 @@ package meh.daniel.com.ricksanchez.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import meh.daniel.com.ricksanchez.App
 import meh.daniel.com.ricksanchez.R
 import meh.daniel.com.ricksanchez.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
+    private val mainViewModel : MainViewModel by viewModels {
+        MainViewModelFactory(App.charactersRepository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
