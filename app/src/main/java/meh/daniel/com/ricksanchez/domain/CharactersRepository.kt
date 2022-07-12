dad28c71df2c2b91fea575bfb384c7e7b7db4aa4
Danil Galkin <mehanikdanil@gmail.com>
2022-07-12 11:20:25 +0700

    добавление в интерфейс метод для получения Api

--- app/src/main/java/meh/daniel/com/ricksanchez/domain/CharactersRepository.kt
+++ app/src/main/java/meh/daniel/com/ricksanchez/domain/CharactersRepository.kt
@@ -1,4 +1,7 @@
 package meh.daniel.com.ricksanchez.domain
 
+import meh.daniel.com.ricksanchez.data.model.CharactersNw
+
 interface CharactersRepository {
+    suspend fun getCharacters() : CharactersNw
 }
