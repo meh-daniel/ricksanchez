d81c7c7c02203c4d6812fde188b29532447dda78
Danil Galkin <mehanikdanil@gmail.com>
2022-07-12 15:12:48 +0700

    метод для определение размеров экрана

--- app/src/main/java/meh/daniel/com/ricksanchez/presentation/MainActivity.kt
+++ app/src/main/java/meh/daniel/com/ricksanchez/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
 package meh.daniel.com.ricksanchez.presentation
 
+import android.content.Context
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Display
 import androidx.activity.viewModels
 import meh.daniel.com.ricksanchez.App
 import meh.daniel.com.ricksanchez.databinding.ActivityMainBinding
@@ -20,7 +23,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         observeViewModel()
+
+        getDisplaySize()
     }
+
+    private fun getDisplaySize() : Int{
+        val display: Display = windowManager.defaultDisplay
+        val point = Point()
+        display.getSize(point)
+        val screenHeight: Int = point.y
+        val height = convertPixelsToDp(this, screenHeight.toFloat())
+        return height.toInt() / 100
+    }
+
+    private fun convertPixelsToDp(context: Context, pixels: Float) =
+        pixels / context.resources.displayMetrics.density
 
     private fun observeViewModel() {
         mainViewModel.charters.observe(this) {
