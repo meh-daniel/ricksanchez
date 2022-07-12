4a75fdd313729ad8c9b124b240289b8d6665335c
Danil Galkin <mehanikdanil@gmail.com>
2022-07-12 11:35:46 +0700

    Реализация ViewModel

---
+++ app/src/main/java/meh/daniel/com/ricksanchez/presentation/MainViewModel.kt
@@ -0,0 +1,25 @@
+package meh.daniel.com.ricksanchez.presentation
+
+import android.util.Log
+import androidx.lifecycle.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import meh.daniel.com.ricksanchez.data.model.CharactersNw
+import meh.daniel.com.ricksanchez.domain.CharactersRepository
+
+class MainViewModel(private val repository: CharactersRepository) : ViewModel() {
+
+    private val _charters : MutableLiveData<List<CharactersNw.Result>> = MutableLiveData()
+    val charters : LiveData<List<CharactersNw.Result>> get() = _charters
+
+    init {
+        viewModelScope.launch(Dispatchers.IO){
+            try {
+                val characters = repository.getCharacters()
+                _charters.postValue(characters.results)
+            } catch (e : Throwable){
+                Log.e("xxx", "error in launcher MainViewModel", e)
+            }
+        }
+    }
+}
