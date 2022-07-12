33159136c21dccf953e26510acbaa2a8bfea7163
Danil Galkin <mehanikdanil@gmail.com>
2022-07-12 12:33:44 +0700

    Получение ответа с сервера

--- app/src/main/java/meh/daniel/com/ricksanchez/presentation/MainActivity.kt
+++ app/src/main/java/meh/daniel/com/ricksanchez/presentation/MainActivity.kt
@@ -2,9 +2,9 @@ package meh.daniel.com.ricksanchez.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import meh.daniel.com.ricksanchez.App
-import meh.daniel.com.ricksanchez.R
 import meh.daniel.com.ricksanchez.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +19,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        mainViewModel.charters.observe(this) {
+            Log.d("xxx", "data: ${it}")
+        }
     }
 }
