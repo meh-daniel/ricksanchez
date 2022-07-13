e9150685e28a3e2c942cd17e03e00b9a1cb39330
Danil Galkin <mehanikdanil@gmail.com>
2022-07-13 13:02:35 +0700

    Конвертирование элементов из modelNw в modelUI

--- app/src/main/java/meh/daniel/com/ricksanchez/presentation/MainViewModel.kt
+++ app/src/main/java/meh/daniel/com/ricksanchez/presentation/MainViewModel.kt
@@ -4,21 +4,35 @@ import android.util.Log
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import meh.daniel.com.ricksanchez.data.model.CharactersNW
 import meh.daniel.com.ricksanchez.domain.CharactersRepository
+import meh.daniel.com.ricksanchez.presentation.modelUI.CharactersUI
+import meh.daniel.com.ricksanchez.presentation.modelUI.ListItem
 
 class MainViewModel(private val repository: CharactersRepository) : ViewModel() {
 
-    private val _charters : MutableLiveData<List<CharactersNW.Result>> = MutableLiveData()
-    val charters : LiveData<List<CharactersNW.Result>> get() = _charters
+    private val _charters : MutableLiveData<List<ListItem>> = MutableLiveData()
+    val charters : LiveData<List<ListItem>> get() = _charters
 
     init {
         viewModelScope.launch(Dispatchers.IO){
             try {
                 val characters = repository.getCharacters()
-                _charters.postValue(characters.results)
+                var listItems = mutableListOf<ListItem>()
+                if (characters.results != null){
+                    for (i in characters.results.indices){
+                        listItems.add(
+                            CharactersUI.Character(
+                                "${characters.results[i].gender}",
+                                "${characters.results[i].image}",
+                                "${characters.results[i].name}"
+                            )
+                        )
+                    }
+                }
+                listItems.add(CharactersUI.Button("NetLoad"))
+                _charters.postValue(listItems)
             } catch (e : Throwable){
-                Log.e("xxx", "error in launcher MainViewModel", e)
+                Log.e("ViewModelLog", "error in launcher MainViewModel", e)
             }
         }
     }
@@ -30,6 +44,6 @@ class MainViewModelFactory(private val repository: CharactersRepository) : ViewM
         if (modelClass.isAssignableFrom(MainViewModel::class.java)){
             return MainViewModel(repository) as T
         }
-        throw IllegalArgumentException("ViewModel class not found")
+        throw IllegalArgumentException("MainViewModel class not found")
     }
 }
