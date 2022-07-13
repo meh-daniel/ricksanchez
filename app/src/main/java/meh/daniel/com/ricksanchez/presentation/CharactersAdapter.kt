1693d8b82116c7de105006342cb859b58956e87e
Danil Galkin <mehanikdanil@gmail.com>
2022-07-13 13:03:06 +0700

    создание адаптера на основе recyclerView

--- app/src/main/java/meh/daniel/com/ricksanchez/presentation/CharactersAdapter.kt
+++ app/src/main/java/meh/daniel/com/ricksanchez/presentation/CharactersAdapter.kt
@@ -2,64 +2,55 @@ package meh.daniel.com.ricksanchez.presentation
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.google.gson.annotations.SerializedName
-import meh.daniel.com.ricksanchez.data.model.CharactersNW
 import meh.daniel.com.ricksanchez.databinding.ItemButtonBinding
 import meh.daniel.com.ricksanchez.databinding.ItemCharacterBinding
+import meh.daniel.com.ricksanchez.presentation.modelUI.CharactersUI
+import meh.daniel.com.ricksanchez.presentation.modelUI.ListItem
 
-private const val CHARACTER_TYPE = 1010
-private const val BUTTON_TYPE = 1100
+class CharactersAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-class CharactersAdapter : ListAdapter<CharactersNW.Result, RecyclerView.ViewHolder>(weatherDiffUtil) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder = when(viewType){
-        CHARACTER_TYPE -> {
-            val binding = ItemCharacterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            CharacterViewHolder(binding)
-        }
-        BUTTON_TYPE -> {
-            val binding = ItemButtonBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            ButtonViewHolder(binding)
-        }
-        else -> {
-            throw IllegalArgumentException("fuck onCreateViewHolder")
-        }
+    private val items = mutableListOf<ListItem>()
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType){
+            ListItem.Type.CHARACTER_TYPE.ordinal-> {
+                val binding = ItemCharacterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                CharacterViewHolder(binding)
+            }
+            ListItem.Type.BUTTON_TYPE.ordinal -> {
+                val binding = ItemButtonBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                ButtonViewHolder(binding)
+            }
+            else ->  throw IllegalArgumentException("fuck onCreateViewHolder")
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when(getItemViewType(position)){
-            CHARACTER_TYPE -> (holder as CharacterViewHolder).bind(item = getItem(position))
-            BUTTON_TYPE -> (holder as ButtonViewHolder)
+        when(holder){
+            is CharacterViewHolder -> holder.bind(items[position] as CharactersUI.Character)
+            is ButtonViewHolder-> holder.bind(items[position] as CharactersUI.Button)
+            else -> throw Exception(" Unknown view type exception")
         }
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return if(true){
-            CHARACTER_TYPE
-        } else{
-            BUTTON_TYPE
+    fun setItems(newItem: List<ListItem>) {
+        items.apply {
+            clear()
+            addAll(newItem)
         }
+        notifyDataSetChanged()
     }
+
+    override fun getItemViewType(position: Int): Int = items[position].getType()
+
+    override fun getItemCount() = items.size
 
 }
 
-//sealed class CharactersUI{
-//    data class Character( val results: List<Result>){
-//        data class Result(
-//            val gender: String,
-//            val image: String,
-//            val name: String
-//        )
-//    }
-//    data class Button(val title: String)
-//}
-
 class CharacterViewHolder(private val binding: ItemCharacterBinding) : RecyclerView.ViewHolder(binding.root){
-    fun bind(item: CharactersNW.Result){
+    fun bind(item: CharactersUI.Character){
         binding.tvName.text = item.name
         binding.tvGender.text = item.gender
         Glide.with(binding.ivAvatar)
@@ -68,17 +59,8 @@ class CharacterViewHolder(private val binding: ItemCharacterBinding) : RecyclerV
     }
 }
 
-class ButtonViewHolder(private val binding: ItemButtonBinding) : RecyclerView.ViewHolder(binding.root)
-
-val weatherDiffUtil = object : DiffUtil.ItemCallback<CharactersNW.Result>() {
-    override fun areItemsTheSame(
-        oldItem: CharactersNW.Result,
-        newItem: CharactersNW.Result
-    ): Boolean = oldItem == newItem
-
-    override fun areContentsTheSame(
-        oldItem: CharactersNW.Result,
-        newItem: CharactersNW.Result
-    ): Boolean = oldItem == newItem
-
+class ButtonViewHolder(private val binding: ItemButtonBinding) : RecyclerView.ViewHolder(binding.root){
+    fun bind(item: CharactersUI.Button){
+        binding.bLoad.text = item.titleName
+    }
 }
